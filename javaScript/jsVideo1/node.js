7e6db39c59f96661b4c0009f732fb77705b849fc
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-07-16 02:37:16 +0530

    java script and node js

---
+++ javaScript/jsVideo1/node.js
@@ -0,0 +1,9 @@
+console.log("jay shree krishna");
+a = 20 ;
+b = 30;
+if(a>b){
+    console.log(a + " is big");
+}else{
+    console.log(b + " is big");
+}
+/* npm : Node Package Manager.*/
---
+++ javaScript/jsVideo1/script.js
@@ -0,0 +1,6 @@
+alert("jay jagannath");
+console.log("it's start now");
+var a = prompt("Enter your number");
+console.log("your number is " + a);
+/* js use in browser is different from node js
+some properties are only avalible in brower */
