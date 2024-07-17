f5d635030ca75b00fcae2640637b2512476dd021
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-07-17 21:11:35 +0530

    js variables

---
+++ javaScript/jsBasic/basic.js
@@ -0,0 +1,11 @@
+console.log("variables");
+//variable using var
+var a = 10;
+console.log(a);
+//let
+let b ="hello";
+console.log(b);
+//var is a global variable but let is block or local variable
+//we mustly use let over var
+const c ="jay";
+//const can't be changed
