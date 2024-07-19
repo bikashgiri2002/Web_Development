9efe038b102d15ae2adfdcdb133c1014070fdbaa
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-07-19 23:27:01 +0530

    businessNameBuilder

---
+++ javaScript/string/index.js
@@ -0,0 +1,10 @@
+//String in js
+let a  = "jay jagannath";
+let b = "biku";
+console.log(a);
+//find length
+let length = a.length;
+console.log(length);
+//temlet literal
+console.log(`i love ${a}`);
+//escape siquence
